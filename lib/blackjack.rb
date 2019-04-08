fbcb1f61793b814744d9d18368c9b7364d688f9b
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-08 15:11:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -13,22 +13,18 @@ def display_card_total(card_total)
 end
 
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
   gets.chomp
 end
 
 def end_game(card_total)
-  # code #end_game here
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
   sum = deal_card + deal_card
   display_card_total(sum)
   sum
